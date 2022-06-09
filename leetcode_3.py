6230f04e45b99eb3cc8516a3311b7b076044365c
Zarif Atai <z.atai@protonmail.com>
2022-06-09 14:44:21 +0200

    Added a more efficient version

--- leetcode_3.py
+++ leetcode_3.py
@@ -13,3 +13,16 @@ class Solution:
                     break
                 max_count = max(max_count, count)
         return max_count
+    
+    def length_of_longest_substring_2(self, s: str) -> int:
+        left = 0
+        max_length = 0
+        chars = set()
+
+        for right, char in enumerate(s):
+            while char in chars:
+                chars.remove(s[left])
+                left += 1
+            chars.add(char)
+            max_length = max(max_length, right - left + 1)
+        return max_length
