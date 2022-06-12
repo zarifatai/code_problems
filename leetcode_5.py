7ecf4058425a1568aacdd5068a48072736267e74
Zarif Atai <z.atai@protonmail.com>
2022-06-12 18:19:25 +0200

    Applied black formatting

--- leetcode_5.py
+++ leetcode_5.py
@@ -2,13 +2,13 @@ class Solution:
     def longest_palindrome(self, s: str) -> str:
         longest_length = 0
         longest_pal = ""
-        
+
         for i in range(len(s)):
             left, right = i, i
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 if (right - left + 1) > longest_length:
                     longest_length = right - left + 1
-                    longest_pal = s[left:right+1]
+                    longest_pal = s[left : right + 1]
                 left -= 1
                 right += 1
 
@@ -16,7 +16,7 @@ class Solution:
             while left >= 0 and right < len(s) and s[left] == s[right]:
                 if (right - left + 1) > longest_length:
                     longest_length = right - left + 1
-                    longest_pal = s[left:right+1]
+                    longest_pal = s[left : right + 1]
                 left -= 1
                 right += 1
         return longest_pal
