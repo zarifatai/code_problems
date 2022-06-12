6ea4eb558d02d1f6c97ee3e0f9a1ef5251f20298
Zarif Atai <z.atai@protonmail.com>
2022-06-12 18:18:27 +0200

    Added solution to leetcode #5

---
+++ leetcode_5.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longest_palindrome(self, s: str) -> str:
+        longest_length = 0
+        longest_pal = ""
+        
+        for i in range(len(s)):
+            left, right = i, i
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if (right - left + 1) > longest_length:
+                    longest_length = right - left + 1
+                    longest_pal = s[left:right+1]
+                left -= 1
+                right += 1
+
+            left, right = i, i + 1
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                if (right - left + 1) > longest_length:
+                    longest_length = right - left + 1
+                    longest_pal = s[left:right+1]
+                left -= 1
+                right += 1
+        return longest_pal
