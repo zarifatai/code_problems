aa3d1ca90b8f2cf26acca6006626bc792fb25294
Zarif Atai <z.atai@protonmail.com>
2022-06-20 01:14:36 +0200

    Applied black formatting

--- leetcode_200.py
+++ leetcode_200.py
@@ -6,11 +6,11 @@ class Solution:
     def num_islands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
-        
+
         rows, cols = len(grid), len(grid[0])
         visited = set()
         res = 0
-        
+
         def bfs(r, c):
             q = deque()
             visited.add((r, c))
@@ -21,7 +21,12 @@ class Solution:
                 directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
                 for dr, dc in directions:
                     r, c = row + dr, col + dc
-                    if r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r, c) not in visited:
+                    if (
+                        r in range(rows)
+                        and c in range(cols)
+                        and grid[r][c] == "1"
+                        and (r, c) not in visited
+                    ):
                         q.append((r, c))
                         visited.add((r, c))
 
@@ -31,7 +36,7 @@ class Solution:
                     bfs(r, c)
                     res += 1
         return res
-    
+
     def num_islands_2(self, grid: List[List[str]]) -> int:
         self.grid = grid
         if not self.grid:
@@ -40,16 +45,22 @@ class Solution:
         count = 0
         for row in range(len(self.grid)):
             for col in range(len(self.grid[0])):
-                if self.grid[row][col] == '1':
+                if self.grid[row][col] == "1":
                     count += 1
                     self.bfs(row, col)
         return count
-    
+
     def bfs(self, row, col):
-        if row < 0 or row >= len(self.grid) or col < 0 or col >= len(self.grid[0]) or self.grid[row][col] != '1':
+        if (
+            row < 0
+            or row >= len(self.grid)
+            or col < 0
+            or col >= len(self.grid[0])
+            or self.grid[row][col] != "1"
+        ):
             return
-        
-        self.grid[row][col] = '2'
+
+        self.grid[row][col] = "2"
         directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
         for dr, dc in directions:
             self.bfs(row + dr, col + dc)
