e9b75f66c3301a25b2e4bf16dfffd85852330f68
Zarif Atai <z.atai@protonmail.com>
2022-06-21 23:39:20 +0200

    Added solution to leetcode #48

---
+++ leetcode_48.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        left, right = 0, len(matrix) - 1
+
+        while left < right:
+            top, bottom = left, right
+
+            for i in range(right - left):
+                top_left = matrix[top][left + i]
+
+                matrix[top][left + i] = matrix[bottom - i][left]
+                matrix[bottom - i][left] = matrix[bottom][right - i]
+                matrix[bottom][right - i] = matrix[top + i][right]
+                matrix[top + i][right] = top_left
+            left += 1
+            right -= 1
+        for row in matrix:
+            print(*row)
